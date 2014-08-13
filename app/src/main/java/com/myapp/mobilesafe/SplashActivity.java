8701db541c9c4821ff0195f5354592f5aaeb4bcb
smg <294594564@qq.com>
2014-08-14 07:45:11 +0800

    mobilesafe

--- app/src/main/java/com/myapp/mobilesafe/SplashActivity.java
+++ app/src/main/java/com/myapp/mobilesafe/SplashActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.animation.AlphaAnimation;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,18 +22,22 @@ import com.lidroid.xutils.http.client.HttpRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+
 public class SplashActivity extends Activity {
 
     private static final String TAG = "myinfo";
     private TextView tv_splash_version;
     private String description;
     private String apkurl;
+    private TextView updateInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         tv_splash_version = (TextView) findViewById(R.id.tp_splash_version);
+        updateInfo = (TextView) findViewById(R.id.tv_update_info);
         tv_splash_version.setText("版本号：" + getVersionName());
         //检查升级
         checkUpdate();
@@ -73,19 +79,55 @@ public class SplashActivity extends Activity {
                                         //版本一致，进入主页面
                                         start();
                                     } else {
-                                        //有新版本，弹出对话框
-                                        AlertDialog builder = new AlertDialog.Builder(SplashActivity.this)
-                                                .setTitle("版本更新")
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this);
+                                        builder.setTitle("更新提示")
                                                 .setIcon(R.drawable.app_icon)
-                                                .setNegativeButton("确认", new DialogInterface.OnClickListener() {
+                                                .setMessage(description)
+                                                .setPositiveButton("立即升级", new DialogInterface.OnClickListener() {
                                                     @Override
                                                     public void onClick(DialogInterface dialogInterface, int i) {
-                                                        Toast.makeText(SplashActivity.this, "确认", Toast.LENGTH_LONG).show();
+                                                        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                                                            Log.i(TAG, apkurl);
+                                                            HttpUtils http = new HttpUtils();
+                                                            http.download(
+                                                                    apkurl,
+                                                                    "/sdcard/mobildsafe.2.0.apk",
+                                                                    true,
+                                                                    true,
+                                                                    new RequestCallBack<File>() {
+                                                                        @Override
+                                                                        public void onLoading(long total, long current, boolean isUploading) {
+                                                                            super.onLoading(total, current, isUploading);
+                                                                            int process = (int) (current * 100 / total);
+                                                                            updateInfo.setText("升级进度：" + process + "%");
+                                                                        }
+
+                                                                        @Override
+                                                                        public void onSuccess(ResponseInfo<File> fileResponseInfo) {
+                                                                            installAPK(fileResponseInfo);
+                                                                        }
+
+                                                                        private void installAPK(ResponseInfo<File> fileResponseInfo) {
+
+                                                                        }
+
+                                                                        @Override
+                                                                        public void onFailure(HttpException e, String s) {
+                                                                            e.getStackTrace();
+                                                                            Toast.makeText(getApplicationContext(), "下载失败", Toast.LENGTH_LONG).show();
+                                                                        }
+                                                                    }
+                                                            );
+                                                        } else {
+                                                            Toast.makeText(getApplicationContext(), "请插入SdCard", Toast.LENGTH_LONG).show();
+                                                            return;
+                                                        }
                                                     }
                                                 })
-                                                .setPositiveButton("取消", new DialogInterface.OnClickListener() {
+                                                .setNegativeButton("下次再说", new DialogInterface.OnClickListener() {
                                                     @Override
                                                     public void onClick(DialogInterface dialogInterface, int i) {
+                                                        dialogInterface.dismiss();
                                                         start();
                                                     }
                                                 })
@@ -99,7 +141,7 @@ public class SplashActivity extends Activity {
 
                             @Override
                             public void onFailure(HttpException e, String s) {
-                                Toast.makeText(SplashActivity.this, "获取版本信息失败", Toast.LENGTH_LONG).show();
+                                Toast.makeText(getApplicationContext(), "获取版本信息失败", Toast.LENGTH_LONG).show();
                             }
                         }
                 );
@@ -108,7 +150,7 @@ public class SplashActivity extends Activity {
     }
 
     private void start() {
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
